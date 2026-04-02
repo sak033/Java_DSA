005328fd2a1df456daaa7ce7d1bfbf2e164ceab3
sak003 <sakshikuthe336@gmail.com>
2026-04-02 22:05:03 +0530

    Questions on patterns

---
+++ Star_patterns/LeftTringle.java
@@ -0,0 +1,14 @@
+class LeftTringle{
+    public static void main(String[] args){
+        /*  Inverted Right triangle
+         
+        for(int i=5; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print('*');
+            }
+            System.out.println(" ");
+        }*/
+
+       for(int )
+    }
+}
