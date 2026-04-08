cc7e5dd233acef0325b55293fa7191488189d64d
sak003 <sakshikuthe336@gmail.com>
2026-04-08 16:24:47 +0530

    My Name using nested loop star pattern

---
+++ Star_patterns/MyName.java
@@ -0,0 +1,146 @@
+class MyName{
+    public static void main(String[] args){
+      
+      for(int i=1; i<=3; i++){
+        for(int j=1; j<=3; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+      }
+     
+       System.out.println();
+
+      for(int i=1; i<=3; i++){
+        for(int j=1; j<=3; j++){
+            if(i==3&&j==2){
+                System.out.print(" ");
+            }
+            else{
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+      }
+      System.out.println();
+
+      for(int i=1; i<=3;i++){
+        for(int j=1; j<=3; j++){
+            if(i==1&&j==2 || i==2&& j==3 || i==3&&j==2){
+                System.out.print(" ");
+            }
+            else{
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+      }
+      System.out.println();
+
+      for(int i=1; i<=3; i++){
+        for(int j=1; j<=3; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+      }
+     
+       System.out.println();
+
+       for(int i=1; i<=3; i++){
+        for(int j=1; j<=3; j++){
+
+            if(i==1&& j==2 || i==3&&j==2){
+                System.out.print(" ");
+            }
+            else{
+              System.out.print("*");
+            }
+           
+        }
+        System.out.println();
+      }
+     
+       System.out.println();
+
+      for(int i=1; i<=3; i++){
+        for(int j=1; j<=3; j++){
+
+            if(i==2&&j==1 || i==2&&j==3){
+                System.out.print(" ");
+            }
+            else{
+                 System.out.print("*");
+            }
+           
+        }
+        System.out.println();
+      } 
+
+      System.out.println();
+
+      for(int i=1; i<=3;i++){
+        for(int j=1; j<=3; j++){
+            if(i==1&&j==2 || i==2&& j==3 || i==3&&j==2){
+                System.out.print(" ");
+            }
+            else{
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+      }
+      System.out.println();
+
+      for(int i=1; i<=3;i++){
+        for(int j=1; j<=3; j++){
+            if(i==1&& j==2 || i==2&& j==2){
+                System.out.print(" ");
+            }
+            else{
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+      }
+      System.out.println();
+
+      for(int i=1; i<=3;i++){
+        for(int j=1; j<=3; j++){
+            if(i==2&& j==1 || i==2&& j==3 || i==3&&j==1 || i==3&& j==3){
+                System.out.print(" ");
+            }
+            else{
+                System.out.print("*");
+            }
+        }
+        System.out.println();
+      }
+      System.out.println();
+      
+      for(int i=1; i<=3; i++){
+        for(int j=1; j<=3; j++){
+
+            if(i==1&& j==2 || i==3&&j==2){
+                System.out.print(" ");
+            }
+            else{
+              System.out.print("*");
+            }
+           
+        }
+        System.out.println();
+      }
+     
+       System.out.println();
+
+       for(int i=1; i<=3; i++){
+        for(int j=1; j<=3; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+      }
+     
+       System.out.println();
+
+
+    }
+}
